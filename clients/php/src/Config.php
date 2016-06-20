a5b67afdfbbc2bf80a891f5c412c97e16dc32bcb
Maksym Tymchyk <reilag69@gmail.com>
2016-06-20 15:34:24 +0300

    20-06-2016 - Init Executer

---
+++ clients/php/src/Config.php
@@ -0,0 +1,21 @@
+<?php namespace Kuna;
+
+
+/**
+ * Class Config
+ * @package Kuna
+ *
+ * Created by PhpStorm.
+ * User: Reilag
+ * Date: 20.06.16
+ * Time: 15:20
+ *
+ */
+class Config
+{
+	const HOST = 'https://kuna.io';
+	const BASE_PATH = "api/v2";
+
+	const MARKET_BTCUAH = 'btcuah';
+
+}
