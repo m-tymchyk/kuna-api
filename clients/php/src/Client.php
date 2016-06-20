274cdc6ff3960cfa18877b1af951f6f8f4a8b51e
Maksym Tymchyk <reilag69@gmail.com>
2016-06-20 14:18:25 +0300

    20-06-2016 - Init PHP API

---
+++ clients/php/src/Client.php
@@ -0,0 +1,23 @@
+<?php namespace Kuna;
+
+/**
+ * Class Client
+ * @package Kuna
+ * @author   https://github.com/reilag/kuna-api
+ * @link     https://github.com/reilag/kuna-api
+ *
+ * Created by PhpStorm.
+ * User: Reilag
+ * Date: 20.06.16
+ * Time: 14:04
+ *
+ */
+class Client
+{
+	const URI = 'https://kuna.io';
+	const BASE_PATH = "/api/v2";
+	
+	
+	
+	
+}
